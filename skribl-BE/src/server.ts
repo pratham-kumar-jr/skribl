50308f1068fade0944d6988d3e192a7266cfd58f
pratham_kumar_jr <pratham.kumar.jr@gmail.com>
2022-04-03 21:32:14 +0530

    redirecting to not found

--- skribl-BE/src/server.ts
+++ skribl-BE/src/server.ts
@@ -10,6 +10,12 @@ const boot = (port: number) => {
       origin: ["http://localhost:3000"],
     })
   );
+  app.get("/", (req, res) => {
+    return res.send("Server is up");
+  });
+  app.all("*", (req, res) => {
+    res.status(404).send("<h1>404! Page not found</h1>");
+  });
   const httpServer = createServer(app);
   webSocketService.init(httpServer);
 
